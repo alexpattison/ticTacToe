f88a7bb7acea1149637fe36ca625d28f435ac321
Alex Pattison <alexlpattison@gmail.com>
2016-10-18 12:24:51 -0700

    Working implementation but it allows you to over index and overwrite

--- ticTacToe.js
+++ ticTacToe.js
@@ -4,11 +4,26 @@ var board = [
   ["_", "_", "_"],
   ["_", "_", "_"]
 ]
-console.log(board);
-var x = process.argv[2];
-var y = process.argv[3];
-var letter = process.argv[4];
+console.log(board[0]);
+console.log(board[1]);
+console.log(board[2]);
 
-board[x][y] = letter;
+var player = 1;
 
-console.log(board);
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    if (player % 2 === 1) {
+      board[chunk[0]][chunk[1]] = 'x';
+    } else {
+      board[chunk[0]][chunk[1]] = 'o';
+    }
+    console.log(board[0]);
+    console.log(board[1]);
+    console.log(board[2]);
+  }
+
+  player += 1;
+});
