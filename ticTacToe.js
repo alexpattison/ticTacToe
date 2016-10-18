8aed884b3b7687b8c7413819e9b1bc5001f3e2f1
Alex Pattison <alexlpattison@gmail.com>
2016-10-18 12:04:21 -0700

    Working but doesnt' render quite like a normal grid

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,1 +1,14 @@
 console.log('Tic-Tac-Toe is up and running');
+var board = [
+  ["_", "_", "_"],
+  ["_", "_", "_"],
+  ["_", "_", "_"]
+]
+console.log(board);
+var x = process.argv[2];
+var y = process.argv[3];
+var letter = process.argv[4];
+
+board[x][y] = letter;
+
+console.log(board);
